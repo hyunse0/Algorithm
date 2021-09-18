30cc64c92c032f591d16b29c2e243dc880e699e3
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2021-09-18 22:39:24 +0900

    210917

---
+++ BEAKJOON/7569.py
@@ -0,0 +1,6 @@
+# 백준 7569 토마토
+
+M, N, H = map(int, input().split())
+box = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+
+print(box)
--- BEAKJOON/teste.py
+++ BEAKJOON/teste.py
@@ -1,9 +1,1 @@
-from collections import deque
-
-a = deque()
-a.append('r')
-a.append('d')
-a.append('s')
-a.append('a')
-
-print(*a)
+print(list(map(int, input().split())))
