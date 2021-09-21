d4db7ba2f2018fd7bf198bde9372be980a00e49a
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2021-09-21 23:51:19 +0900

    210921

--- BEAKJOON/7569.py
+++ BEAKJOON/7569.py
@@ -1,6 +1,37 @@
 # 백준 7569 토마토
 
+from collections import deque
+
+def bfs(k, i, j):
+    queue = deque()
+    queue.append((k, i, j))
+
+    visited = [[[0 * M] for _ in range(N)] for _ in range(H)]
+    visited[k][i][j] = 1
+
+    dxyz = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+
+    while queue:
+        z, x, y = queue.popleft()
+
+        for dz, dx, dy in dxyz:
+            nz, nx, ny = z+dz, x+dx, y+dy
+
+            if 0 <= nz < H and 0 <= nx < M and 0 <= ny < N and not visited[nz][nx][ny]:
+                visited[nz][nx][ny] = 1
+                queue.append((nz, nx, ny))
+
+                if box[nz][nx][ny] == 1:
+                    if box[z][x][y] == 0:
+                        box[z][x][y] = 1
+
+    return
+
+
 M, N, H = map(int, input().split())
 box = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
+print(box)
+cnt = 0
+bfs(0, 0, 0)
 print(box)
