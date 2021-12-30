b9a9d8b38affccb8780e9a5f67e13a98efe8dbf6
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2021-12-30 23:12:21 +0900

    211230

--- PROGRAMMERS/test.py
+++ PROGRAMMERS/test.py
@@ -1,3 +1,53 @@
-prices = [1, 8, 1, 8, 1, 8]
+from collections import defaultdict
 
-print(len(prices)%2)
+def solution(schedule):
+    def select(day):
+        nonlocal week
+
+        if len(day) > 8:
+            d1, t1, d2, t2 = day.split()
+
+            h1, m1 = t1.split(':')
+            h2, m2 = t2.split(':')
+
+            time1 = int(h1)*60 + int(m1)
+            time2 = int(h2)*60 + int(m2)
+
+            for _ in range(3):
+                if time1 in week[d1]:
+                    return False
+                
+                if time2 in week[d2]:
+                    return False
+
+                week[d1].append(time1)
+                week[d2].append(time2)
+                time1 += 30
+                time2 += 30
+        else:
+            d, t = day.split()
+            h, m = t.split(':')
+            time = int(h)*60 + int(m)
+            for _ in range(6):
+                if time in week[d]:
+                    return False
+
+                week[d].append(time)
+                time += 30
+        
+        return True
+
+    no = False
+    for c1 in schedule[0]:
+        week = defaultdict(list)
+        can = [[] for _ in range(5)]
+        select(c1)
+        for c2 in schedule[1]:
+            if not select(c2):
+                no = True
+                break
+            else:
+                can[1].append()
+
+    answer = -1
+    return answer
