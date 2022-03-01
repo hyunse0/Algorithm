5f26a4a141fc204645d791f26111ae561e67d445
SSAFY <gkgustj@naver.com>
2022-03-01 23:55:22 +0900

    220301

--- BEAKJOON/13460.py
+++ BEAKJOON/13460.py
@@ -18,6 +18,17 @@ def move(x, y, dx, dy):
             return (x-dx, y-dy)
 
 
+def restore(red, mr, blue, mb):
+    global board
+
+    board[red[0]][red[1]] = 'R'
+    if mr != (0, 0) and red != mr:
+        board[mr[0]][mr[1]] = '.'
+    board[blue[0]][blue[1]] = 'B'
+    if mb != (0, 0) and blue != mb:
+        board[mb[0]][mb[1]] = '.'
+
+
 def game(cnt, red, blue):
     global board, answer
 
@@ -29,68 +40,22 @@ def game(cnt, red, blue):
         if board[red[0]+dxy[i][0]][red[1]+dxy[i][1]] not in ('.', 'O') and board[blue[0]+dxy[i][0]][blue[1]+dxy[i][1]] not in ('.', 'O'):
             continue
 
-        # 아래로 이동 (아래쪽에 있는 공부터 움직이도록)
-        if i == 0:
-            if red[0] < blue[0]:
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-            else:
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
-        # 왼쪽 이동
-        elif i == 1:
-            if red[1] < blue[1]:
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
-            else:
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-        # 위로 이동
-        elif i == 2:
-            if red[0] < blue[0]:
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
-            else:
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-        # 오른쪽 이동
+        # 해당 방향쪽에 있는 공부터 움직이도록
+        if (i == 0 and red[0] < blue[0]) or (i == 1 and blue[1] < red[1]) or (i == 2 and blue[0] < red[0]) or (i == 3 and red[1] < blue[1]):
+            mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
+            mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
         else:
-            if red[1] < blue[1]:
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-            else:
-                mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
-                mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
+            mr = move(red[0], red[1], dxy[i][0], dxy[i][1])
+            mb = move(blue[0], blue[1], dxy[i][0], dxy[i][1])
 
         # 빨간 공은 구멍으로 빠져나가고 파란 공은 빠져나가지 않은 경우
         if mr == (0, 0) and mb != (0, 0):
             answer = min(answer, cnt)
-
-            # 되돌리기
-            board[red[0]][red[1]] = 'R'
-            board[blue[0]][blue[1]] = 'B'
-            if blue != mb:
-                board[mb[0]][mb[1]] = '.'
-            
-        # 파랑 공이 구멍으로 빠져나간 경우
-        elif mb == (0, 0):
-            # 되돌리기
-            board[red[0]][red[1]] = 'R'
-            if mr != (0, 0) and red != mr:
-                board[mr[0]][mr[1]] = '.'
-            board[blue[0]][blue[1]] = 'B'
-
         # 두 공 모두 구멍으로 빠져나가지 못한 경우
-        else:
+        elif mb != (0, 0):
             game(cnt+1, mr, mb)
             
-            # 되돌리기
-            board[red[0]][red[1]] = 'R'
-            if red != mr:
-                board[mr[0]][mr[1]] = '.'
-            board[blue[0]][blue[1]] = 'B'
-            if blue != mb:
-                board[mb[0]][mb[1]] = '.'
+        restore(red, mr, blue, mb)
 
 
 N, M = map(int, input().split())
