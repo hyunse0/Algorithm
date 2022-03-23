374778991896016fca26a5e9f0e6a5bb198bbdc3
SSAFY <gkgustj@naver.com>
2022-03-23 23:40:29 +0900

    220322

--- BEAKJOON/19236.py
+++ BEAKJOON/19236.py
@@ -1,48 +1,45 @@
 # 백준 19236 청소년 상어
 
-def move_fish(num, way):
-    new_space = [[0 for _ in range(4)] for _ in range(4)]
-    new_numbers = {}
+import copy
 
+def move_fish(num, way, space, numbers, shark):
     x, y = numbers[num]
 
     while True:
         nx, ny = x+dxy[way][0], y+dxy[way][1]
-        if 0 <= nx < 4 and 0 <= ny < 4 and not visited[nx][ny]:
-            new_space[nx][ny] = (num, way)
-            new_space[x][y] = space[nx][ny]
-            new_numbers[space[x][y][0]] = (x, y)
-            new_numbers[space[nx][ny][0]] = (nx, ny)
-            return new_space, new_numbers
+        if 0 <= nx < 4 and 0 <= ny < 4 and (nx, ny) != shark:
+            space[x][y], space[nx][ny] = space[nx][ny], space[x][y]
+            numbers[space[x][y][0]] = (x, y)
+            numbers[space[nx][ny][0]] = (nx, ny)
+            return space, numbers
         else:
             if way == 7: way -= 8
             way += 1
 
 
-def move_shark(eat, space, numbers):
-    global shark, way, visited, answer
+def move_shark(x, y, way, eat, before, numbers):
+    global visited, answer
 
+    after = copy.deepcopy(before)
+    after_numbers = numbers.copy()
+    
     # 상어가 물고기를 잡아먹음
-    eat += space[shark[0]][shark[1]][0]
-    way = space[shark[0]][shark[1]][1]
-    numbers.pop(space[shark[0]][shark[1]][0])
-    space[shark[0]][shark[1]] = 0
-    visited[shark[0]][shark[1]] = 1
+    eat += after[x][y][0]
+    way = after[x][y][1]
+    after_numbers.pop(after[x][y][0])
+    after[x][y] = 0
+    visited[x][y] = 1
 
     # 물고기 이동
-    for num, fish in numbers.items():
-        new_space, new_numbers = move_fish(num, space[fish[0]][fish[1]][1])
+    for num, fish in after_numbers.items():
+        after, after_numbers = move_fish(num, after[fish[0]][fish[1]][1], after, after_numbers, (x, y))
     
     # 상어 이동
     while True:
-        x, y = shark[0], shark[1]
         nx, ny = x+dxy[way][0], y+dxy[way][1]
 
         if 0 <= nx < 4 and 0 <= ny < 4 and space[nx][ny][0]:
-            shark = (nx, ny)
-            move_shark(eat, new_space, new_numbers)
-            shark = (x, y)
-            
+            move_shark(nx, ny, way, eat, after, after_numbers)
         else:
             answer = max(answer, eat)
             return
@@ -64,9 +61,6 @@ for i in range(4):
 dxy = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
 visited = [[0 for _ in range(4)] for _ in range(4)]
 
-shark = (0, 0)
-way = (-1, -1)
-
 answer = 0
-move_shark(0, space, numbers)
+move_shark(0, 0, -1, 0, space, numbers)
 print(answer)
