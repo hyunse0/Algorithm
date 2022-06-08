09b229f1abcf02b34104496965473cd895a933e0
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2022-06-09 03:23:09 +0900

    220608

--- BEAKJOON/2477.py
+++ BEAKJOON/2477.py
@@ -2,49 +2,26 @@
 
 K = int(input())
 
-x = []
-y = []
+lengths = []
 
+max_even = max_odd = even_idx = odd_idx = -1
+
+# 어차피 가로-세로-가로-세로-... 순으로 정렬되어있기 때문에 가로, 세로를 구분할 필요x
 for i in range(6):
     way, length = map(int, input().split())
 
-    if way == 1 or way == 2:
-        y.append((i, length))
-    else:
-        x.append((i, length))
+    if not i%2 and max_even < length:
+        max_even = length
+        even_idx = i
+    elif i%2 and max_odd < length:
+        max_odd = length
+        odd_idx = i
+    
+    lengths.append(length)
 
-x.sort(key=lambda z: -z[1])
-y.sort(key=lambda z: -z[1])
+# '홀수 번째 중 가장 큰 변'과 양 옆으로 맞닿아있는 변들의 차 = 짝수 번째 중 작은 사각형의 길이
+sub_even = abs(lengths[odd_idx-1] - lengths[(odd_idx+1)%6])
+sub_odd = abs(lengths[even_idx-1] - lengths[(even_idx+1)%6])
 
-mx = x[0][1]
-mxi = x[0][0]
-
-my = y[0][1]
-myi = y[0][0]
-
-mxim = mxi - 1
-mxip = mxi + 1
-
-myim = myi - 1
-myip = myi + 1
-
-if mxi == 0:
-    mxim = 5
-elif mxi == 5:
-    mxip = 0
-
-if myi == 0:
-    myim = 5
-elif myi == 5:
-    myip = 0
-
-for i, s in x[1:]:
-    if i != myim and i != myip:
-        sx = s
-
-for i, s in y[1:]:
-    if i != mxim and i != mxip:
-        sy = s
-
-width = (mx * my) - (sx * sy)
+width = (max_even * max_odd) - (sub_even * sub_odd)
 print(K * width)
