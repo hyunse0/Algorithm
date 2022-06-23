b8fc28e526d6415195ed6134e094423d296c4fa2
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2022-06-24 03:46:19 +0900

    PG 메뉴 리뉴얼 - 뮨자열

--- PROGRAMMERS/메뉴 리뉴얼.py
+++ PROGRAMMERS/메뉴 리뉴얼.py
@@ -1,27 +1,37 @@
+# 프로그래머스 메뉴 리뉴얼
+
 from collections import defaultdict
+from itertools import combinations
 
 def solution(orders, course):
-    order = defaultdict(list)
-    
-    for menus in orders:
-        for i in range(len(menus)):
-            for j in range(i+1, len(menus)):
-                order[menus[i]].append(menus[j])
-    
-    answer = []
-    for key, value in order.items():
-        for cnt in course:
-            temp = [key]
-            
-            for v in set(value):
-                if value.count(v) == cnt:
-                    temp.append(v)
-            
-            if len(temp) >= 2:
-                temp.sort()
-                answer.append(''.join(temp))
-    
-    return sorted(list(set(answer)))
+    orders.sort(key=lambda x: -len(x))
+    course.sort()
 
-orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+    answer = []
+    for cnt in course:
+        while orders and len(orders[-1]) < cnt:
+            orders.pop()
+        
+        menus = defaultdict(int)
+        for order in orders:
+            for combo in combinations(order, cnt):
+                combo = sorted(list(combo))
+                menus[''.join(combo)] += 1
+
+        result = sorted(menus.items(), key=lambda x: -x[1])
+        
+        i = 0
+        while i < len(result) and result[0][1] >= 2 and result[i][1] == result[0][1]:
+            answer.append(result[i][0])
+            i += 1
+    
+    return sorted(answer)
+
+# orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+# print(solution(orders, [2, 3, 4]))
+
+# orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+# print(solution(orders, [2, 3, 5]))
+
+orders = ["XYZ", "XWY", "WXA"]
 print(solution(orders, [2, 3, 4]))
