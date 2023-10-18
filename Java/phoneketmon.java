04f8ec216c13688ee7d53e29e665e30724a40bd0
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2023-10-19 00:15:36 +0900

    phoneketmon 수정

--- Java/phoneketmon.java
+++ Java/phoneketmon.java
@@ -2,16 +2,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public int solution(int[] nums) {
-      int maxCnt = (nums.length)/2;
-      Set<Integer> set = new HashSet();
-      
-      for (int num : nums) {
-        set.add(num);
-      }
-
-      int uniqueNumCnt = set.size();
-      
-      return (maxCnt < uniqueNumCnt) ? maxCnt : uniqueNumCnt;
+  public int solution(int[] nums) {
+    int maxCnt = (nums.length)/2;
+    Set<Integer> set = new HashSet();
+    
+    for (int num : nums) {
+      set.add(num);
     }
+
+    int uniqueNumCnt = set.size();
+    
+    return (maxCnt < uniqueNumCnt) ? maxCnt : uniqueNumCnt;
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.solution(new int[]{3, 1, 2, 3}));
+  }
 }
+  
+
