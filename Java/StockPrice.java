9b56c25ef0ea03af5f28fa2eac6262fff9b0b14f
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2024-02-19 00:14:31 +0900

    StockPrice_array

---
+++ Java/StockPrice.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockPrice {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i+1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[j] < prices[i]) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
