0ae5fa4b7d76c1b700db874390b6443ac7ea860e
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2024-02-18 23:47:26 +0900

    Truck_Queue

---
+++ Java/Truck.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Truck {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        int answer = 0;
+        int i = 0;
+        int queueSum = 0;
+
+        while (i < truck_weights.length) {
+            answer++;
+
+            if (queue.size() == bridge_length) {
+                queueSum -= queue.remove();
+            }
+
+            if (queueSum + truck_weights[i] <= weight) {
+                queueSum += truck_weights[i];
+                queue.add(truck_weights[i]);
+                i++;
+            } else {
+                queue.add(0);
+            }
+        }
+
+        if (queueSum > 0) {answer += bridge_length;}
+
+        return answer;
+}
